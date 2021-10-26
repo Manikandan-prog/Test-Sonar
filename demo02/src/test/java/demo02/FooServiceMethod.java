6483472178950fe63148874d2709402d8ff12021
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-26 13:21:01 +0530

    Update FooServiceMethod.java

--- demo02/src/test/java/demo02/FooServiceMethod.java
+++ demo02/src/test/java/demo02/FooServiceMethod.java
@@ -1,56 +1,10 @@
 package demo02;
 
 
-
-//import junit.framework.Test;
-//import junit.framework.TestCase;
-//import junit.framework.TestSuite;
-//
-//
-//
-///**
-// * Unit test for simple App.
-// */
-//public class AppTest 
-//    extends TestCase
-//{
-//    /**
-//     * Create the test case
-//     *
-//     * @param testName name of the test case
-//     */
-//    public AppTest( String testName )
-//    {
-//        super( testName );
-//    }
-//
-//    /**
-//     * @return the suite of tests being tested
-//     */
-//    public static Test suite()
-//    {
-//        return new TestSuite( AppTest.class );
-//    }
-//
-//    /**
-//     * Rigourous Test :-)
-//     */
-//    public void testApp()
-//    {
-//        assertTrue( true );
-//    }
-//}
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
-
 import com.demo.FooService;
-
 import nl.altindag.log.LogCaptor;
-
-
-
 
 public class FooServiceMethod {
 
@@ -67,12 +21,12 @@ public class FooServiceMethod {
         LogCaptor logCaptor = LogCaptor.forClass(FooService.class);
         FooService fooService = new FooService();
 
-       // fooService.sayHello();
+       
 
 
         assertThat(logCaptor.getInfoLogs().contains(expectedInfoMessage));
 
-        //assertThat(logCaptor.getWarnLogsrnLogs.containsExactly(expectedWarnMessage));
+       
 
    }
 
